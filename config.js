faed10f32835177136fdaa3573a932bee6ce98da
Emma Rose 🌹 <contactemmarose247@gmail.com>
2026-07-29 05:15:20 +0100

    Add GROQ_API_KEY to configuration

--- config.js
+++ config.js
@@ -24,6 +24,7 @@ module.exports = {
     OWNER_NAME: process.env.OWNER_NAME || "Mirage",
     BOT_NAME: process.env.BOT_NAME || "Kord",
     RENDER_API_KEY: process.env.RENDER_API_KEY,
+    GROQ_API_KEY: process.env.GROQ_API_KEY || "",
     ANTIDELETE: process.env.ANTIDELETE || "on",
     ANTIDELETE_INCHAT: process.env.ANTIDELETE_INCHAT || "off",
     ANTI_EDIT: process.env.ANTI_EDIT || "off",
